0b516c5137a39b8570e2a3fa545a958eeae07f1d
lxh <pigpigfangfang@gmail.com>
2023-06-29 10:17:16 +0800

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace BubbleSortExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {64, 34, 25, 12, 22, 11, 90};
+            
+            Console.WriteLine("Original array:");
+            foreach (int item in arr)
+                Console.Write(item + " ");
+            
+            BubbleSort(arr);
+            
+            Console.WriteLine("\nSorted array:");
+            foreach (int item in arr)
+                Console.Write(item + " ");
+        }
+        
+        static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // swap arr[j] and arr[j+1]
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
